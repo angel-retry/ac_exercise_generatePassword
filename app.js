1c8029919968446a07d93b44dc8db59b1cf0c07b
a27657060 <a27657060@gmail.com>
2023-07-24 13:42:55 +0800

    edit validated.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
@@ -9,8 +10,16 @@ app.set('views', './views');
 
 app.use(express.static('public'))
 
+// 設定 body-parser 中間件
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.post('/generator_password', (req, res) => {
+  const data = req.body
+  res.send(data.passwordLength)
 })
 
 app.listen(port, () => {
